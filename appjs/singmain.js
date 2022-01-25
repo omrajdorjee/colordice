9e70961cceb4ff6579168faa1943e0ecd706de99
deViom <91524273+omrajdorjee@users.noreply.github.com>
2022-01-25 15:45:42 +0530

    Update singmain.js

--- appjs/singmain.js
+++ appjs/singmain.js
@@ -2,19 +2,23 @@
 var imageTracker = 'playImage';
 //set events handlers for on click
 document.getElementById("swapImage").onclick = function() {
-	swapImage('song');
+  swapImage('song');
 	playPause('song');
+
 };
 document.getElementById("autoPlay-2").onclick = function() {
-		swapImage('songPl3');
+	swapImage('songPl3');
 	playPause('songPl3');
+
 };
 document.getElementById("autoPlay-3").onclick = function() {
-		swapImage('songPl2');
+	swapImage('songPl2');
 	playPause('songPl2');
+
 };
 document.getElementById("stopImage").onclick = function() {
 	stop();
+  
 }
 document.getElementById("nextImage").onclick = function() {
 	forward();
@@ -40,7 +44,7 @@ function Buttontoggle()
   if (imageTracker == 'playImage') {
   $("button").addClass("pills");
     $(".tempo").addClass("conseal"); 
- document.body.style.backgroundColor = '#77D970';
+ document.body.style.backgroundColor = '#9AD0EC';
   } else {
     imageTracker = 'playImage';
   }
@@ -84,24 +88,30 @@ var musicTracker = 'noMusic';
 //active track
 var activeTrack = 0;
 
-
+let playplay = "";
 var playPause = function(playList) {
-     let playplay= playList;
-     
+    
+    playplay = playList;    
   if (playplay == 'song' && musicTracker == 'noMusic') {
   	audios[activeTrack].play();
     musicTracker = 'playMusic';
     
     
+    $('.asw-2, .asw-3').addClass("tglHide");
+    return playplay
   } 
   else if(playplay == 'songPl2' && musicTracker == 'noMusic'){
-    playList2[activeTrack].play();
+    playList2[activeTrack].play();   
     musicTracker = 'playMusic';
+    playplay = 'songPl2';
+    $('.asw-1, .asw-3').addClass("tglHide");
   }
   
   else if(playplay == 'songPl3' && musicTracker == 'noMusic'){
     playList3[activeTrack].play();
     musicTracker = 'playMusic';
+    playplay = 'songPl3';
+    $('.asw-2, .asw-1').addClass("tglHide");
   }
   
   else {
@@ -109,11 +119,14 @@ var playPause = function(playList) {
      audios[activeTrack].pause();
     playList2[activeTrack].pause();
     playList3[activeTrack].pause();
+    
       document.body.style.backgroundColor = '#D3DEDC';
+      $('.asw-2, .asw-3, .asw-1').removeClass("tglHide");
        $("button").removeClass("pills");
     musicTracker = 'noMusic';
   }
   showPlaying();
+  return playplay;
 };
 
 var stop = function() {
@@ -127,24 +140,42 @@ var stop = function() {
   }
 };
 
+
 var forward = function(){
-  function increment(){
-  	 if (activeTrack < audios.length - 1)
+  function increment(value){
+  	 if (activeTrack < value.length - 1)
      		activeTrack++;
      else activeTrack = 0;
   }
-	if (musicTracker == 'playMusic') {
+	if (playplay == 'song' &&  musicTracker == 'playMusic') {
   	 audios[activeTrack].pause();
 		 audios[activeTrack].currentTime = 0;
      
-     increment();
+     increment(audios);
   	 audios[activeTrack].play();
+     
+  }
+	if (playplay == 'songPl2' && musicTracker == 'playMusic') {
+  	 playList2[activeTrack].pause();
+		 playList2[activeTrack].currentTime = 0;
+     
+     increment(playList2);
+  	 playList2[activeTrack].play();
+    
+     
+  }
+	if (playplay == 'songPl3' && musicTracker == 'playMusic') {
+  	 playList3[activeTrack].pause();
+		 playList3[activeTrack].currentTime = 0;
+     
+     increment(playList3);
+  	 playList3[activeTrack].play();
      
   }
    else {
     increment();
   }
-  showPlaying();
+  
 };
 
 var backward = function(){
@@ -153,37 +184,62 @@ var backward = function(){
      		activeTrack--;
      else activeTrack = audios.length -1;
   }
-	if (musicTracker == 'playMusic') {
+	if (playplay == 'song' &&  musicTracker == 'playMusic') {
   	 audios[activeTrack].pause();
-		 audios[activeTrack].currentTime = 0;
-     decrement();
+		 audios[activeTrack].currentTime = 0;  
+     decrement(audios);
   	 audios[activeTrack].play();
-  } else {
+     
+  }
+	if (playplay == 'songPl2' && musicTracker == 'playMusic') {
+  	 playList2[activeTrack].pause();
+		 playList2[activeTrack].currentTime = 0;    
+     decrement(playList2);
+  	 playList2[activeTrack].play();
+    
+     
+  }
+	if (playplay == 'songPl3' && musicTracker == 'playMusic') {
+  	 playList3[activeTrack].pause();
+		 playList3[activeTrack].currentTime = 0;
+     
+     decrement  (playList3);
+  	 playList3[activeTrack].play();
+     
+  }
+  else {
     decrement();
   }
   showPlaying();
 };
-
 var showPlaying = function()
 {
 	var src = audios[activeTrack].src;
-           
-   $(".song").removeClass("playing");
-   $(".song").addClass("playing");
-   console.log( $(".song"));
+   $(".audio").removeClass("playing");
+   $('.audio').addClass("playing");
+   console.log(src);
 };
 
 
-
-
-document.getElementById('stopBtn').onclick = function stopAllAudio() {
+function stopAllAudio() {
+  if (playplay == 'song') {
+  document.getElementById("swapImage").click(); 
+}
+else if (playplay == 'songPl2') {
+  document.getElementById("autoPlay-3").click(); 
+}
+else if (playplay == 'songPl3'){
+  document.getElementById("autoPlay-2").click(); 
+}
     var sounds = document.getElementsByTagName('audio');
+    for (i = 0; i < sounds.length; i++) sounds[i].pause();
     $("button").removeClass("pills");
     $(".tempo").removeClass("conseal"); 
-    playPause('song');
-    playPause('songPl2');
-    playPause('songPl3');
-    for (i = 0; i < sounds.length; i++) sounds[i].pause();
+    $('.asw-2, .asw-3').removeClass("tglHide");
+    document.body.style.backgroundColor = '#D3DEDC';
+    playPause('else');
+
+
 };
 
 
